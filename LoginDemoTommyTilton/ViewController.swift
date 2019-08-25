b8ef17e592ade7c7abc6bf4aef641e39970cfbce
Thomas Tilton <ttilton17@gmail.com>
2019-08-24 21:50:31 -0400

    Initial Commit

---
+++ LoginDemoTommyTilton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginDemoTommyTilton
+//
+//  Created by Thomas Tilton on 8/24/19.
+//  Copyright © 2019 Thomas Tilton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
